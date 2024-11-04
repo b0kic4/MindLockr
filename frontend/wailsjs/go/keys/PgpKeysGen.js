e4a3861a14e01e0dae92383a4cb47723012b9c9c
Boris Nikolic <borisnikolic2302@gmail.com>
2024-11-04 14:34:12 +0100

    feat: gen without saving pgp keys test: enc and dec

--- frontend/wailsjs/go/keys/PgpKeysGen.js
+++ frontend/wailsjs/go/keys/PgpKeysGen.js
@@ -6,14 +6,22 @@ export function DecryptPgpPrivKey(arg1, arg2) {
   return window['go']['keys']['PgpKeysGen']['DecryptPgpPrivKey'](arg1, arg2);
 }
 
-export function GenerateEcPgpKeys(arg1) {
-  return window['go']['keys']['PgpKeysGen']['GenerateEcPgpKeys'](arg1);
+export function GenECC(arg1) {
+  return window['go']['keys']['PgpKeysGen']['GenECC'](arg1);
+}
+
+export function GenRSA(arg1) {
+  return window['go']['keys']['PgpKeysGen']['GenRSA'](arg1);
+}
+
+export function GenStoreECC(arg1) {
+  return window['go']['keys']['PgpKeysGen']['GenStoreECC'](arg1);
+}
+
+export function GenStoreRSA(arg1) {
+  return window['go']['keys']['PgpKeysGen']['GenStoreRSA'](arg1);
 }
 
 export function GeneratePGPKeys(arg1) {
   return window['go']['keys']['PgpKeysGen']['GeneratePGPKeys'](arg1);
-}
-
-export function GenerateRsaPgpKeys(arg1) {
-  return window['go']['keys']['PgpKeysGen']['GenerateRsaPgpKeys'](arg1);
 }
